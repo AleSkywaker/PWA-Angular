1ae95968aefc7a351cabdfc9dda9edb4cc49a4d8
Alex Colombo <uricaine@hotmail.com>
2019-05-09 15:09:49 +0200

    new component paises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PaisComponent } from './pages/pais/pais.component';
+import { PaisesComponent } from './pages/paises/paises.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaisComponent
+    PaisComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule

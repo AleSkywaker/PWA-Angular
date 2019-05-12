ac360ec0982390702f573778adae2c7e3ce4fa81
Alex Colombo <uricaine@hotmail.com>
2019-05-12 09:53:12 +0100

    import hhtclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-import { PaisComponent } from './pages/pais/pais.component';
-import { PaisesComponent } from './pages/paises/paises.component';
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppComponent } from "./app.component";
+import { PaisComponent } from "./pages/pais/pais.component";
+import { PaisesComponent } from "./pages/paises/paises.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PaisComponent,
-    PaisesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PaisComponent, PaisesComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

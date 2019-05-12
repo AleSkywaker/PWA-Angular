d738f7216d5de4ed5d288121bdd0d7fdd137d899
Alex Colombo <uricaine@hotmail.com>
2019-05-12 09:53:56 +0100

    init pais service

--- src/app/services/paises.service.ts
+++ src/app/services/paises.service.ts
@@ -1,9 +1,20 @@
+import { PaisInterface } from './../interfaces/pais.interface';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  private paises: PaisInterface[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  getPaises(){
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+     .subscribe(  (paises:PaisInterface[]) =>{
+        console.log(paises)
+     })
+  }
 }

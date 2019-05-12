db35be519ddbc8d9581864a3e8ed88195df69cdd
Alex Colombo <uricaine@hotmail.com>
2019-05-12 10:28:30 +0100

    creating service

--- src/app/services/paises.service.ts
+++ src/app/services/paises.service.ts
@@ -1,25 +1,29 @@
-import { PaisInterface } from './../interfaces/pais.interface';
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { PaisInterface } from "./../interfaces/pais.interface";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PaisesService {
-
+  
   private paises: PaisInterface[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getPaises(): Promise<PaisInterface[]>{
-
-    if (this.getPaises.length > 0){
-       return Promise.resolve(this.paises)
+  getPaises(): Promise<PaisInterface[]> {
+    if (this.getPaises.length > 0) {
+      return Promise.resolve(this.paises);
     }
-    this.http.get('https://restcountries.eu/rest/v2/lang/es')
-     .subscribe(  (paises:PaisInterface[]) =>{
-        console.log(paises);
-        this.paises = paises;
-     })
+
+    return new Promise(resolve => {
+      this.http
+        .get("https://restcountries.eu/rest/v2/lang/es")
+        .subscribe((paises: PaisInterface[]) => {
+          console.log(paises);
+          this.paises = paises;
+          resolve(paises);
+        });
+    });
   }
 }

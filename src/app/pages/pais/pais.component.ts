fb120a67b58b86fd776e75d5dd4b1dd2b1be8923
Alex Colombo <uricaine@hotmail.com>
2019-05-13 22:54:06 +0200

    working with pais component

--- src/app/pages/pais/pais.component.ts
+++ src/app/pages/pais/pais.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisesService } from '../../services/paises.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-pais',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaisComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public paisesService : PaisesService,
+    private activatedRoute : ActivatedRoute,
+    private router : Router
+  ) { }
 
   ngOnInit() {
   }
